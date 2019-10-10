a264295ddf143c3db8ecd4da82584ba030b706fe
Ennes13 <56233563+Ennes13@users.noreply.github.com>
2019-10-09 23:02:48 -0300

    Add files via upload

---
+++ cg-tp2/Codigos/draws.h
@@ -0,0 +1,145 @@
+// Desenho do jogo
+void desenhaMinhaCena()
+{
+    glColor4f(1, 1, 1, 1);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
+    if(tela == 0)
+    {
+        drawObject(wallpaper_menu);
+        drawObject(jogar);
+        drawObject(controles);
+        drawObject(creditos);
+        drawObject(sair_menu);
+    }
+
+    else if(tela == 1)
+    {
+        movimentacao();
+
+        // Coordenadas da camera em coordenadas esfericas
+        camera.x = 100*sin(pi)*cos(theta);
+        camera.y = 100*cos(pi);
+        camera.z = 100*sin(pi)*sin(theta);
+
+        glLoadIdentity();
+
+        switch(modoCamera)
+        {
+            // Camera simples
+            default:
+
+                gluLookAt(0, 0, 200, 0, 0, 0, 0, 1, 0);
+                break;
+
+            // Camera primeira pessoa
+            case 2:
+
+                gluLookAt(cursor.x + 0,cursor.y + 0, cursor.z + 0, cursor.x + camera.x,cursor.y + camera.y, cursor.z + camera.z, 0, 1, 0);
+                break;
+
+            // Camera terceira pessoa
+            case 3:
+
+                gluLookAt(cursor.x + camera.x,cursor.y + camera.y, cursor.z + camera.z, cursor.x + 0,cursor.y + 0, cursor.z + 0, 0, 1, 0);
+                break;
+        }
+
+        // INICIO DO TESTE
+
+        glClearColor(1,1,1,1);
+		
+		if (isLightingOn) {
+        glEnable(GL_LIGHTING);
+    	}
+
+		glPushMatrix();
+	    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+		glmDraw(teste, GLM_SMOOTH | GLM_TEXTURE | GLM_COLOR);
+   		glPopMatrix();
+
+		glPushMatrix();
+		glTranslatef( 1, -50, 1);
+		glColor4f(0, 1, 0, 1);
+		glmDraw(terreno, GLM_SMOOTH | GLM_TEXTURE | GLM_COLOR);
+   		glPopMatrix();
+
+        glPushMatrix();
+		glColor4f(0, 1, 0, 1);       //começa a desenhar com a cor verde
+		glTranslatef(0, -100, 0);   //desenha no ponto Y=-100 para parecer uma mesa para a jarra
+		glutSolidCube(100);        //desenha um cubo no ponto (0, -100, 0)
+		glColor4f(0, 0, 0, 1);    //começa a desenhar com a cor preta
+		glutWireCube(105);       //desenha bordas de um cubo levemente maior
+        glPopMatrix();
+
+		if (isLightingOn) {
+        glDisable(GL_LIGHTING);
+    	}
+        // FIM DO TESTE
+
+		desenhaInterface();
+    }
+
+    else if(tela == 2)
+    {
+
+    }
+
+    else if(tela == 3)
+    {
+        drawObject(wallpaper_creditos);
+        drawObject(sair_creditos);
+    }
+    
+    glutSwapBuffers();
+}
+
+//Desenha os objetos e imagens
+void drawObject(spriteObject objeto)
+{
+
+    glEnable(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D, objeto.textura);
+
+    glPushMatrix();
+    glTranslatef(objeto.posicao.x, objeto.posicao.y, 0);
+
+    glBegin(GL_TRIANGLE_FAN);
+    {
+        glTexCoord2f(0, 0);
+        glVertex3f(-objeto.dimensoes.x/2, -objeto.dimensoes.y/2,  0);
+        glTexCoord2f(1, 0);
+        glVertex3f( objeto.dimensoes.x/2, -objeto.dimensoes.y/2,  0);
+        glTexCoord2f(1, 1);
+        glVertex3f( objeto.dimensoes.x/2,  objeto.dimensoes.y/2,  0);
+        glTexCoord2f(0, 1);
+        glVertex3f(-objeto.dimensoes.x/2,  objeto.dimensoes.y/2,  0);
+    }
+
+    glEnd();
+    glPopMatrix();
+    glDisable(GL_TEXTURE_2D);
+}
+
+void desenhaInterface(){	
+        //perguntar como fazer isso
+}
+
+/*
+void desenhaRodaGigante(){
+    int i;
+    float angulo = 360/CARRINHOS;
+    glPushMatrix();
+    glTranslatef(rodaGigante.posicao.x, rodaGigante.posicao.y, rodaGigante.posicao.z);
+    //desenha o aro e base da roda gigante
+    for(i=0, i<qtdeCarrinhos, i++){
+        int aux = i+1;
+        glPushMatrix();
+        glTranslatef(rodaGigante.raio*cos(angulo)*aux, rodaGigante.raio*sin(angulo)*aux, rodaGigante.posicao.z);
+        //desenha carrinho
+        glPopMatrix();
+    }
+    glPopMatrix();
+}
+
+*/
---
+++ cg-tp2/Codigos/setup.h
@@ -0,0 +1,109 @@
+﻿// Inicializa comandos essenciais
+void setup()
+{
+    // Cor do fundo
+    glClearColor(1.0, 1.0, 1.0, 0.0);
+
+    // Habilita a utilização de texturas
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    // Limpa a tela
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // Desativando a opção de repetir uma tecla quando ela é continua apertada
+    glutSetKeyRepeat(0);
+
+    // Maquinas de estado
+    tela = 0;
+    cursor.x = 0;
+    cursor.y = 0;
+    cursor.z = 0;
+
+	if(!teste)
+	{
+    	teste = glmReadOBJ("../pontos_obj/arvore.obj");
+		glmUnitize(teste);
+		glmScale(teste, 50.0);
+		glmFacetNormals(teste);
+		glmVertexNormals(teste, 90.0, 1);
+	}
+
+	if(!terreno)
+	{
+    	terreno = glmReadOBJ("../pontos_obj/floor.obj");
+		glmUnitize(terreno);
+		glmScale(terreno, 50.0);
+		glmFacetNormals(terreno);
+		glmVertexNormals(terreno, 90.0, 0);
+	}
+	
+    wallpaper_menu.textura = loadTexture("../Imagens/wallpaper_menu.png");
+    jogar.textura = loadTexture("../Imagens/jogar_branco.png");
+    controles.textura = loadTexture("../Imagens/controles_branco.png");
+    creditos.textura = loadTexture("../Imagens/creditos_branco.png");
+    sair_menu.textura = loadTexture("../Imagens/sair_branco.png");
+    sair_creditos.textura = loadTexture("../Imagens/sair_branco.png");
+    wallpaper_creditos.textura = loadTexture("../Imagens/wallpaper_creditos.png");
+
+    GLfloat lPos[4]={0.00, 0.00, 0.00, 1.00 };
+    GLfloat lKa[4]={0.00, 0.5, 0.00, 1.00};
+    GLfloat lKd[4]={0.5, 0.5, 0.5, 1.00};
+    GLfloat lKs[4]={1.00, 1.00, 1.00, 1.00};
+    GLfloat mKd[4]={0,0,0,1};
+    GLfloat mKs[4]={0,0,0,1};
+    GLfloat mKe[4]={0,0,0,1};
+
+    GLfloat lKa1[4]={1.0, 1.0, 1.0, 1.00};       // cor ambiente (basta mudar as 3 para escolher a cor da luz)
+    GLfloat lKd1[4]={1.0, 1.0, 1.0, 1.00};       // cor difusa
+    GLfloat lKs1[4]={1.00, 1.00, 1.00, 1.00};    // cor especular
+
+    GLfloat lPos1[4]={7.00, -31.2, -45.9, 1.00 };
+    GLfloat lPos2[4]={7.00, -31.2, -70.7, 1.00 };
+    GLfloat lPos3[4]={7.00, -31.2, -96.6, 1.00 };
+    GLfloat lPos4[4]={-16.7, -31.2, -16.8, 1.00 };
+    GLfloat lPos5[4]={-133.3, -31.2, 37.2, 1.00 };
+
+    float matAmbAndDif[4] = {1.0, 1.0, 1.0, 1.0};    // vetor cor ambiente e difusa: branca
+    //float matShine[] = {5};                          // expoente especular (shininess)
+    int light = 0;
+}
+
+void attPosicao()
+{
+    // Objetos gerais
+    wallpaper_menu.dimensoes.x = largura;
+    wallpaper_menu.dimensoes.y = altura;
+    wallpaper_menu.posicao.x = largura/2;
+    wallpaper_menu.posicao.y = altura/2;
+
+    jogar.dimensoes.x = largura/6;
+    jogar.dimensoes.y = altura/12;
+    jogar.posicao.x = largura/5;
+    jogar.posicao.y = altura/2 - altura/50;
+
+    controles.dimensoes.x = largura/4 - largura/50;
+    controles.dimensoes.y = altura/12;
+    controles.posicao.x = largura/6;
+    controles.posicao.y = altura/4;
+
+    creditos.dimensoes.x = largura/4 - largura/50;
+    creditos.dimensoes.y = altura/12;
+    creditos.posicao.x = largura/2 + largura/3;
+    creditos.posicao.y = altura/2 - altura/50;
+
+    sair_menu.dimensoes.x = largura/8;
+    sair_menu.dimensoes.y = altura/12;
+    sair_menu.posicao.x = largura/2 + largura/3 - largura/20;
+    sair_menu.posicao.y = altura/4;
+
+    sair_creditos.dimensoes.x = largura/8;
+    sair_creditos.dimensoes.y = altura/12;
+    sair_creditos.posicao.x = largura/2 + largura/3;
+    sair_creditos.posicao.y = altura/4;
+
+    wallpaper_creditos.dimensoes.x = largura;
+    wallpaper_creditos.dimensoes.y = altura;
+    wallpaper_creditos.posicao.x = largura/2;
+    wallpaper_creditos.posicao.y = altura/2;
+}
