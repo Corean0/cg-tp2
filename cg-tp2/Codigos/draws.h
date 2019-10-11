289b6e71e01fc7a0c53cecae9bd2caa6dac3f522
Ennes13 <56233563+Ennes13@users.noreply.github.com>
2019-10-10 21:24:29 -0300

    Add files via upload

---
+++ cg-tp2/Codigos/draws.h
@@ -0,0 +1,179 @@
+// Desenho do jogo
+void desenhaMinhaCena()
+{
+    glMatrixMode(GL_MODELVIEW);
+    glColor4f(1, 1, 1, 1);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
+    if(tela == 0)
+    {
+        drawObject(wallpaper_menu);
+        drawObject(jogar);
+        drawObject(controles);
+        drawObject(creditos);
+        drawObject(sair_menu);
+    }
+
+    else if(tela == 1)
+    {
+        movimentacao();
+
+        // Coordenadas da camera em coordenadas esfericas
+        camera.x = 100*sin(pi)*cos(theta);
+        camera.y = 100*cos(pi);
+        camera.z = 100*sin(pi)*sin(theta);
+
+        glLoadIdentity();
+
+        switch(modoCamera)
+        {
+            // Camera simples
+            default:
+
+                gluLookAt(0, 0, 200, 0, 0, 0, 0, 1, 0);
+                break;
+
+            // Camera primeira pessoa
+            case 2:
+
+                gluLookAt(cursor.x + 0,cursor.y + 0, cursor.z + 0, cursor.x + camera.x,cursor.y + camera.y, cursor.z + camera.z, 0, 1, 0);
+                break;
+
+            // Camera terceira pessoa
+            case 3:
+
+                gluLookAt(cursor.x + camera.x,cursor.y + camera.y, cursor.z + camera.z, cursor.x + 0,cursor.y + 0, cursor.z + 0, 0, 1, 0);
+                break;
+        }
+
+    // Esfera indicativa de onde está a luz
+/*	glPushMatrix();
+        glTranslatef(20.0, 50.0, 50.0);
+        glColor3f(0, 0, 0);
+        glutWireSphere(2, 8, 8); 
+    glPopMatrix();
+*/
+		
+	if (isLightingOn)
+	{
+        glEnable(GL_LIGHTING);
+    }
+    
+		// INICIO DO TESTE
+	    	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+		glClearColor(1,1,1,1);
+
+		// Teste de Arvore
+		glPushMatrix();
+			glTranslatef(-20,0,0);
+			glScalef(50,50,50);
+	    		//glCallList(teste.listaVisualizacao);
+			glmDraw(testeOBJ, GLM_SMOOTH | GLM_TEXTURE | GLM_COLOR);
+	   	glPopMatrix();
+
+		// Teste de pedra
+		glPushMatrix();
+			glTranslatef(40,-30,0);
+			glScalef(20,20,20);
+	    		//glCallList(pedra.listaVisualizacao);
+			glmDraw(pedraOBJ, GLM_SMOOTH | GLM_TEXTURE | GLM_COLOR);
+	   	glPopMatrix();
+
+		// Teste de Terreno com lista
+		glPushMatrix();
+		glTranslatef(0,-50,0);
+		for(int random = -1, auxrandom, random1, auxrandom1 ; random <= 1 ;random++)
+		{
+			auxrandom = random*50;
+			for(random1 = -1 ;random1 <= 1; random1++)
+			{
+				auxrandom1 = random1*50;
+				glPushMatrix();
+					glTranslatef(auxrandom,0,auxrandom1);
+					glScalef(50,50,50);
+	    				//glCallList(terreno.listaVisualizacao);
+					glmDraw(terrenoOBJ, GLM_SMOOTH | GLM_TEXTURE | GLM_COLOR);
+				glPopMatrix();
+			}
+		}
+		glPopMatrix();
+
+	if (isLightingOn)
+    {
+    	glDisable(GL_LIGHTING);
+    }
+
+		// FIM DO TESTE
+	//desenhando e rotacionando roda gigante
+	/*
+	auxRotacao+=1;
+	glPushMatrix();
+		glRotate(auxRotacao,0,0,1);
+		desenhaRodaGigante();
+	glPopMatrix();
+	*/
+	desenhaInterface();
+    }
+
+    else if(tela == 2)
+    {
+
+    }
+
+    else if(tela == 3)
+    {
+        drawObject(wallpaper_creditos);
+        drawObject(sair_creditos);
+    }
+    
+    glutSwapBuffers();
+}
+
+//Desenha os objetos e imagens
+void drawObject(spriteObject objeto)
+{
+
+    glEnable(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D, objeto.textura);
+
+    glPushMatrix();
+    glTranslatef(objeto.posicao.x, objeto.posicao.y, 0);
+
+    glBegin(GL_TRIANGLE_FAN);
+    {
+        glTexCoord2f(0, 0);
+        glVertex3f(-objeto.dimensoes.x/2, -objeto.dimensoes.y/2,  0);
+        glTexCoord2f(1, 0);
+        glVertex3f( objeto.dimensoes.x/2, -objeto.dimensoes.y/2,  0);
+        glTexCoord2f(1, 1);
+        glVertex3f( objeto.dimensoes.x/2,  objeto.dimensoes.y/2,  0);
+        glTexCoord2f(0, 1);
+        glVertex3f(-objeto.dimensoes.x/2,  objeto.dimensoes.y/2,  0);
+    }
+
+    glEnd();
+    glPopMatrix();
+    glDisable(GL_TEXTURE_2D);
+}
+
+void desenhaInterface(){	
+        //perguntar como fazer isso
+}
+
+
+void desenhaRodaGigante(){
+    int i;
+    float angulo = 360/CARRINHOS;
+    float raio = rodaGigante.dimensoes.y/2;
+    glPushMatrix();
+	    glTranslatef(rodaGigante.posicao.x, rodaGigante.posicao.y, rodaGigante.posicao.z);
+	    //desenha o aro e base da roda gigante
+	    for(i=0; i<CARRINHOS; i++){
+		glPushMatrix();
+		glTranslatef(raio*cos(angulo)*i, raio*sin(angulo)*i, 0);
+		//desenha carrinho
+		glPopMatrix();
+	    }
+    glPopMatrix();
+}
---
+++ cg-tp2/Codigos/setup.h
@@ -0,0 +1,84 @@
+﻿// Inicializa comandos essenciais
+void setup()
+{
+    // Cor do fundo
+    glClearColor(1.0, 1.0, 1.0, 0.0);
+
+    // Habilita a utilização de texturas
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    // Limpa a tela
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // Desativando a opção de repetir uma tecla quando ela é continua apertada
+    glutSetKeyRepeat(0);
+
+    // Maquinas de estado
+    tela = 0;
+    cursor.x = 0;
+    cursor.y = 0;
+    cursor.z = 0;
+
+    // carregando objetos	
+    testeOBJ = glmReadOBJ("../pontos_obj/arvore_1.obj");
+    terrenoOBJ = glmReadOBJ("../pontos_obj/terreno.obj");
+	pedraOBJ = glmReadOBJ("../pontos_obj/pedra_1.obj");
+
+    // carregando listas (não carrega nada na lista (??) );	
+    carregaOBJ(teste, testeOBJ);
+    carregaOBJ(terreno, terrenoOBJ);
+	carregaOBJ(pedra, pedraOBJ);
+    //carregaOBJ(rodaGigante);
+	
+    wallpaper_menu.textura = loadTexture("../Imagens/wallpaper_menu.png");
+    jogar.textura = loadTexture("../Imagens/jogar_branco.png");
+    controles.textura = loadTexture("../Imagens/controles_branco.png");
+    creditos.textura = loadTexture("../Imagens/creditos_branco.png");
+    sair_menu.textura = loadTexture("../Imagens/sair_branco.png");
+    sair_creditos.textura = loadTexture("../Imagens/sair_branco.png");
+    wallpaper_creditos.textura = loadTexture("../Imagens/wallpaper_creditos.png");
+
+    float matAmbAndDif[4] = {1.0, 1.0, 1.0, 1.0};    // vetor cor ambiente e difusa: branca
+    float matShine[] = {5};                          // expoente especular (shininess)
+    int light = 0;
+}
+
+void attPosicao()
+{
+    // Objetos gerais
+    wallpaper_menu.dimensoes.x = largura;
+    wallpaper_menu.dimensoes.y = altura;
+    wallpaper_menu.posicao.x = largura/2;
+    wallpaper_menu.posicao.y = altura/2;
+
+    jogar.dimensoes.x = largura/6;
+    jogar.dimensoes.y = altura/12;
+    jogar.posicao.x = largura/5;
+    jogar.posicao.y = altura/2 - altura/50;
+
+    controles.dimensoes.x = largura/4 - largura/50;
+    controles.dimensoes.y = altura/12;
+    controles.posicao.x = largura/6;
+    controles.posicao.y = altura/4;
+
+    creditos.dimensoes.x = largura/4 - largura/50;
+    creditos.dimensoes.y = altura/12;
+    creditos.posicao.x = largura/2 + largura/3;
+    creditos.posicao.y = altura/2 - altura/50;
+
+    sair_menu.dimensoes.x = largura/8;
+    sair_menu.dimensoes.y = altura/12;
+    sair_menu.posicao.x = largura/2 + largura/3 - largura/20;
+    sair_menu.posicao.y = altura/4;
+
+    sair_creditos.dimensoes.x = largura/8;
+    sair_creditos.dimensoes.y = altura/12;
+    sair_creditos.posicao.x = largura/2 + largura/3;
+    sair_creditos.posicao.y = altura/4;
+
+    wallpaper_creditos.dimensoes.x = largura;
+    wallpaper_creditos.dimensoes.y = altura;
+    wallpaper_creditos.posicao.x = largura/2;
+    wallpaper_creditos.posicao.y = altura/2;
+}
